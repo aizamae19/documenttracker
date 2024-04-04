2d91e42020b1b7247fc459d796e692452aa1150f
Aiza Genol <genolaiza@gmail.com>
2024-04-04 10:37:55 +0700

    added leave application

--- resources/views/admin/files/leaveform.blade.php
+++ resources/views/admin/files/leaveform.blade.php
@@ -128,9 +128,184 @@
                                                     <input type="checkbox" class="my-checkbox" value="1" />
                                                     Adoption Leave (R.A. No. 8552)
                                                 </div>
+                                                <div class="line">
+                                                    <p style="font-size: 15px;"><i>Others:</i></p>
+                                                </div>
+                                                <div class="line">
+                                                    <input type="text" style="width: 100%; padding: 5px 5px; box-sizing: border-box; border: none; border-bottom: 1px solid #000;">
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <div class="form-group">
+                                                <div class="line">
+                                                    <p>6.A DETAILS OF LEAVE</p>
+                                                </div>
+                                                <div class="line">
+                                                    <p style="font-size: 15px;"><i>In case of Vacation/Special Privilege Leave:</i></p>
+                                                </div>   
+                                                <div class="line">
+                                                    <input type="checkbox" class="my-checkbox" value="1" />
+                                                    Within the Philippines
+                                                    <input type="text" style="width: 45%; padding: 5px 5px; box-sizing: border-box; border: none; border-bottom: 1px solid #000;">
+                                                </div>  
+                                                <div class="line">
+                                                    <input type="checkbox" class="my-checkbox" value="1" />
+                                                    Abroad (Specify)
+                                                    <input type="text" style="width: 55%; padding: 5px 5px; box-sizing: border-box; border: none; border-bottom: 1px solid #000;">
+                                                </div>  
+                                                <div class="line">
+                                                    <p style="font-size: 15px;"><i>In case of Sick Leave:</i></p>
+                                                </div>
+                                                <div class="line">
+                                                    <input type="checkbox" class="my-checkbox" value="1" />
+                                                    In Hospital (Specify Illness)
+                                                    <input type="text" style="width: 33%; padding: 5px 5px; box-sizing: border-box; border: none; border-bottom: 1px solid #000;">
+                                                </div>  
+                                                <div class="line">
+                                                    <input type="checkbox" class="my-checkbox" value="1" />
+                                                    Out Patient (Specify Illness)
+                                                    <input type="text" style="width: 31%; padding: 5px 5px; box-sizing: border-box; border: none; border-bottom: 1px solid #000;">
+                                                </div>
+                                                <div class="line">
+                                                    <p style="font-size: 15px;"><i>In case of Special Leave Benefits for Women:</i></p>
+                                                </div>  
+                                                <div class="line">
+                                                    (Specify Illness)
+                                                    <input type="text" style="width: 62%; padding: 5px 5px; box-sizing: border-box; border: none; border-bottom: 1px solid #000;">
+                                                </div>
+                                                <div class="line">
+                                                    <p style="font-size: 15px;"><i>In case of Study Leave:</i></p>
+                                                </div>
+                                                <div class="line">
+                                                    <input type="checkbox" class="my-checkbox" value="1" />
+                                                    Completion of Master's Degree
+                                                </div>  
+                                                <div class="line">
+                                                    <input type="checkbox" class="my-checkbox" value="1" />
+                                                    BAR/Board Examination Review
+                                                </div>
+                                                <div class="line">
+                                                    <p style="font-size: 15px;"><i>Other purpose:</i></p>
+                                                </div>
+                                                <div class="line">
+                                                    <input type="checkbox" class="my-checkbox" value="1" />
+                                                    Monetization of Leave Credits
+                                                </div>  
+                                                <div class="line">
+                                                    <input type="checkbox" class="my-checkbox" value="1" />
+                                                    Terminal Leave 
+                                                </div>    
                                             </div>
                                         </div>
                                     </div>
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="card-header">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-8">
+                                            <div class="form-group">
+                                                <p>6.C NUMBER OF WORKING DAYS APPLIED FOR</p>
+                                                <div class="line">
+                                                    <input type="text" style="width: 100%; padding: 5px 5px; box-sizing: border-box; border: none; border-bottom: 1px solid #000;">
+                                                </div>
+                                                <div class="line">
+                                                    INCLUSIVE DATES
+                                                </div>
+                                                <div class="line">
+                                                    <input type="text" style="width: 100%; padding: 5px 5px; box-sizing: border-box; border: none; border-bottom: 1px solid #000;">
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <div class="form-group">
+                                                <p>6.D COMMUTATION </p>
+                                                <div class="line">
+                                                    <input type="checkbox" class="my-checkbox" value="1" />
+                                                    Not Requested
+                                                </div>  
+                                                <div class="line">
+                                                    <input type="checkbox" class="my-checkbox" value="1" />
+                                                    Requested
+                                                </div>  
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="card-header">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="card-header">
+                                                <label style="text-align: center;">7. DETAILS OF ACTION ON APPLICATION</label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-8">
+                                            <div class="form-group">
+                                                <p>7.A CERTIFICATION OF LEAVE CREDITS</p>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <div class="form-group">
+                                                <p>7.B RECOMMENDATION</p>
+                                                <div class="line">
+                                                    <input type="checkbox" class="my-checkbox" value="1" />
+                                                    For Approval
+                                                </div>  
+                                                <div class="line">
+                                                    <input type="checkbox" class="my-checkbox" value="1" />
+                                                    For disapproval due to
+                                                    <input type="text" style="width: 45%; padding: 5px 5px; box-sizing: border-box; border: none; border-bottom: 1px solid #000;">
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="card-header">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-8">
+                                            <div class="form-group">
+                                                <p>7.C APPROVED FOR:</p>
+                                            </div>
+                                            <div class="line">
+                                                <input type="text" style="width: 7%; padding: 2px 3px; box-sizing: border-box; border: none; border-bottom: 1px solid #000;">
+                                                days with pay
+                                            </div>
+                                            <div class="line">
+                                                <input type="text" style="width: 7%; padding: 2px 3px; box-sizing: border-box; border: none; border-bottom: 1px solid #000;">
+                                                days without pay
+                                            </div>
+                                            <div class="line">
+                                                <input type="text" style="width: 7%; padding: 2px 3px; box-sizing: border-box; border: none; border-bottom: 1px solid #000;">
+                                                others (Specify)
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <div class="form-group">
+                                                <p>7.D DISAPPROVED DUE TO:</p>
+                                                <input type="text" style="width: 100%; padding: 5px 5px; box-sizing: border-box; border: none; border-bottom: 1px solid #000;">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="card-header">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <br>
                                     <div class="col-md-12">
                                         <a href="{{route('filedashboard')}}" class="btn btn-danger" data-dismiss="modal">Cancel</a>
                                         <button type="submit" class="btn btn-primary">Save</button>

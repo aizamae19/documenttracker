7bf569b539601731812165b317107523f34b6ff0
Aiza Genol <genolaiza@gmail.com>
2024-04-12 08:52:14 +0700

    modified

---
+++ database/migrations/2024_04_12_014139_create_travelorders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('travelorders', function (Blueprint $table) {
+            $table->id();
+            $table->string('Date');
+            $table->string('Subject');
+            $table->string('Location');
+            $table->string('InclusiveDates');
+            $table->string('Name');
+            $table->string('Designation');
+            $table->string('Office');
+            $table->string('Endorser');
+            $table->string('Dated');
+            $table->string('Purpose');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('travelorders');
+    }
+};

bedab9c610a1fb7320baa578d5fd1e75ceae17ca
rosemie <ellaglaizalayan1233@gmail.com>
2024-04-18 08:18:50 +0800

    updated

--- resources/views/admin/files/index.blade.php
+++ resources/views/admin/files/index.blade.php
@@ -175,111 +175,114 @@
                         </table>
                      </div>
                   </div>
-<div id="travelorderTab" class="container-fluid tab-pane">
-    <div class="col-md-12">
-        <table id="itemSearchTab" class="table table-hover">
-            <thead>
-                <tr>
-                    <th>Series Number</th>
-                    <th>Office</th>
-                    <th>Name</th>
-                    <th>Date</th>
-                    <th class="text-center">Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                @if(isset($travelorders))
-                    @foreach($travelorders as $key => $travelorder)
-                        <tr>
-                            <?php 
-                            $SeriesNumber = sprintf('%06d', $key + 1); 
-                            
-                            // Fetch the office code for the current office
-                            $office = App\Models\Office::where('OfficeName', $travelorder->Office)->first();
-                            $officeCode = $office ? $office->Code : '';
-                            ?>
-<td>{{ date('Y') . '-' . $officeCode . '-' . $SeriesNumber }}</td>
-                            <td>{{ $travelorder->Office }}</td>
-                            <td>{{ $travelorder->NameOfEmployee }}</td>
-                            <td>{{ substr($travelorder->created_at, 0, 10) }}</td>
-                            <td class="text-center">
-                                <a class="btn btn-sm btn-success" href="" data-toggle="modal" data-target="#view"><i class="fa fa-eye"></i> View</a>
-                                <a class="btn btn-sm btn-danger" href="" data-toggle="modal" data-target="#delete"><i class="fa fa-trash-alt"></i> Delete</a>
-                            </td>
-                        </tr>
-                    @endforeach
-                @endif
-            </tbody>
-        </table>
-    </div>
-</div>
-
-      <div id="delete" class="modal animated rubberBand delete-modal" role="dialog">
-            <div class="modal-dialog modal-dialog-centered">
-                <div class="modal-content">
-                   <div class="modal-body text-center">
-                       <img src="{{asset('assets/img/sent.png')}}" alt="" width="50" height="46">
-                       <h3>Are you sure want to delete this file?</h3>
-                       <div class="m-t-20"> <a href="#" class="btn btn-white" data-dismiss="modal">Close</a>
-                          <button type="submit" class="btn btn-danger">Delete</button>
-                       </div>
-                    </div>
-                 </div>
-              </div>
-           </div>
-         <div id="add" class="modal animated rubberBand delete-modal" role="dialog">
-            <div class="modal-dialog modal-dialog-centered modal-lg">
-               <div class="modal-content">
-                  <div class="modal-body text-center">
-                     <div class="card-body">
-                        <div class="row">
-                           <div class="col-md-12">
-                              <div class="card-header">
-                                 <h5><img src="{{asset('assets/img/file.png')}}" width="30"> Document Information</h5>
-                              </div>
-                              <br>
-                           <div class="slide-v">
-                              <div class="slide">
-                                 <div class="row">
-                                    <div class="col-md-6">
-                                       <div class="form-group">
-                                          <div class="card-footer">
-                                             <a href="{{ route('applicationforleave') }}" class="form-control link" id="applicationforleaveLink">APPLICATION FOR LEAVE</a>
+               <div id="travelorderTab" class="container-fluid tab-pane">
+                  <div class="col-md-12">
+                     <table id="itemSearchTab" class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th>Series Number</th>
+                                <th>Office</th>
+                                <th>Name</th>
+                                <th>Date</th>
+                                <th class="text-center">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @if(isset($travelorders))
+                                @foreach($travelorders as $key => $travelorder)
+                                    <tr>
+                                        <?php 
+                                        $SeriesNumber = sprintf('%06d', $key + 1); 
+                                        
+                                        $office = App\Models\Office::where('OfficeName', $travelorder->Office)->first();
+                                        $officeCode = $office ? $office->Code : '';
+                                        ?>
+                                        <td>{{ date('Y') . '-' . $officeCode . '-' . $SeriesNumber }}</td>
+                                        <td>{{ $travelorder->Office }}</td>
+                                        <td>{{ $travelorder->FileType }}</td>
+                                        <td>{{ substr($travelorder->created_at, 0, 10) }}</td>
+                                        <td class="text-center">
+                                            <a class="btn btn-sm btn-success" href="" data-toggle="modal" data-target="#view"><i class="fa fa-eye"></i>View</a>
+                                            <a class="btn btn-sm btn-danger" href="" data-toggle="modal" data-target="#delete"><i class="fa fa-trash-alt"></i>Delete</a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            @endif
+                           </tbody>
+                       </table>
+                   </div>
+               </div>
+               <div id="delete" class="modal animated rubberBand delete-modal" role="dialog">
+                     <div class="modal-dialog modal-dialog-centered">
+                         <div class="modal-content">
+                            <div class="modal-body text-center">
+                                <img src="{{asset('assets/img/sent.png')}}" alt="" width="50" height="46">
+                                <h3>Are you sure want to delete this file?</h3>
+                                <div class="m-t-20"> <a href="#" class="btn btn-white" data-dismiss="modal">Close</a>
+                                   <button type="submit" class="btn btn-danger">Delete</button>
+                               </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                     <div id="add" class="modal animated rubberBand delete-modal" role="dialog">
+                        <div class="modal-dialog modal-dialog-centered modal-lg">
+                           <div class="modal-content">
+                              <div class="modal-body text-center">
+                                 <div class="card-body">
+                                    <div class="row">
+                                       <div class="col-md-12">
+                                          <div class="card-header">
+                                             <h5><img src="{{asset('assets/img/file.png')}}" width="30"> Document Information</h5>
                                           </div>
-                                       </div>
-                                    </div>
-                                    <div class="col-md-6">
-                                       <div class="form-group">
-                                          <div class="card-footer">
-                                             <a href="{{ route('certificateofappearance')}}" class="form-control" id="certificateofappearanceLink">CERTIFICATE OF APPEARANCE</a>
-                                          </div>
-                                       </div>
-                                    </div>
-                                    <div class="col-md-6">
-                                       <div class="form-group">
-                                          <div class="card-footer">
-                                             <a href="{{ route('tripticket')}}" class="form-control" id="tripticketLink">TRIP TICKET</a>
-                                          </div>
-                                       </div>
-                                    </div>
-                                    <div class="col-md-6">
-                                       <div class="form-group">
-                                          <div class="card-footer">
-                                             <a href="{{ route('getDispatch') }}" class="form-control" id="dispatchLink">DISPATCH</a>
-                                          </div>
-                                       </div>
-                                    </div>
-                                    <div class="col-md-6">
-                                       <div class="form-group">
-                                          <div class="card-footer">
-                                             <a href="{{ route('locator')}}" class="form-control" id="locatorLink">LOCATOR</a>
-                                          </div>
-                                       </div>
-                                    </div>
-                                    <div class="col-md-6">
-                                       <div class="form-group">
-                                          <div class="card-footer">
-                                             <a href="{{ route('travelorder')}}" class="form-control" id="travelorderLink">TRAVEL ORDER</a>
+                                          <br>
+                                          <div class="slide-v">
+                                             <div class="slide">
+                                                <div class="row">
+                                                   <div class="col-md-6">
+                                                      <div class="form-group">
+                                                         <div class="card-footer">
+                                                            <a href="{{ route('applicationforleave') }}" class="form-control link" id="applicationforleaveLink">APPLICATION FOR LEAVE</a>
+                                                         </div>
+                                                      </div>
+                                                   </div>
+                                                   <div class="col-md-6">
+                                                      <div class="form-group">
+                                                         <div class="card-footer">
+                                                            <a href="{{ route('certificateofappearance')}}" class="form-control" id="certificateofappearanceLink">CERTIFICATE OF APPEARANCE</a>
+                                                         </div>
+                                                      </div>
+                                                   </div>
+                                                   <div class="col-md-6">
+                                                      <div class="form-group">
+                                                         <div class="card-footer">
+                                                            <a href="{{ route('tripticket')}}" class="form-control" id="tripticketLink">TRIP TICKET</a>
+                                                         </div>
+                                                      </div>
+                                                   </div>
+                                                   <div class="col-md-6">
+                                                      <div class="form-group">
+                                                         <div class="card-footer">
+                                                            <a href="{{ route('getDispatch') }}" class="form-control" id="dispatchLink">DISPATCH</a>
+                                                         </div>
+                                                      </div>
+                                                   </div>
+                                                   <div class="col-md-6">
+                                                      <div class="form-group">
+                                                         <div class="card-footer">
+                                                            <a href="{{ route('locator')}}" class="form-control" id="locatorLink">LOCATOR</a>
+                                                         </div>
+                                                      </div>
+                                                   </div>
+                                                   <div class="col-md-6">
+                                                      <div class="form-group">
+                                                         <div class="card-footer">
+                                                            <a href="{{ route('travelorder')}}" class="form-control" id="travelorderLink">TRAVEL ORDER</a>
+                                                         </div>
+                                                      </div>
+                                                   </div>
+                                                </div>
+                                             </div>
                                           </div>
                                        </div>
                                     </div>
@@ -288,9 +291,4 @@
                            </div>
                         </div>
                      </div>
-                  </div>
-               </div>
-            </div>
-         </div>
-      </div>
 @endsection 
